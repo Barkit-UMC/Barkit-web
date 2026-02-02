d1c6e508c7eaf50b2dac85e0ef1875f23e8fd13e
이소연 <122617471+silviasylee03@users.noreply.github.com>
2026-02-02 15:11:47 +0900

    fix: 없는 페이지 제거

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import ChangeBirthdayPage from './pages/profile/ChangeBirthdayPage';
 import AddToHomePage from './pages/profile/AddToHomePage';
 import LogoutPage from './pages/profile/LogoutPage';
 import UnscribePage from './pages/profile/UnscribePage';
-import LocationPermissionPage from './pages/profile/LocationPermissionPage';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 
