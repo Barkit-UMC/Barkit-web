daf7107e86e43051db83385d4bdccd3b54019fea
이소연 <122617471+silviasylee03@users.noreply.github.com>
2026-02-15 14:32:33 +0900

    fix: 테두리

--- src/pages/map/MapHomePage.tsx
+++ src/pages/map/MapHomePage.tsx
@@ -285,7 +285,7 @@ export default function MapHomePage() {
                                     !px-5 !py-2 text-base rounded-full font-medium whitespace-nowrap transition-colors
                                     ${selectedCategory === category
                                         ? 'bg-white text-gray-900 ring-1 ring-[#00C0E8]'
-                                        : 'bg-white text-gray-500 hover:bg-gray-50 ring-[#00C0E8]'}
+                                        : 'bg-white text-gray-500 hover:bg-gray-50'}
                                 `}
                             >
                                 {category}
